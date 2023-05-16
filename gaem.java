e76a7fe1dc4e053b44ecc3cf272e64a234b7e0ff
mintoya <119896751+mintoya@users.noreply.github.com>
2023-05-16 15:23:17 -0400

    iy4PIUGAWE[G'
    gwasdga

--- gaem.java
+++ gaem.java
@@ -1,14 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
 
 public class gaem extends Thread {
-    private final speed speed = new speed();
-    private final field gField;
+    private speed speed = new speed();
+    private field gField;
+    JFrame frame;
+    private int high = 0;
+    private int current = 0;
 
     public gaem(){
-
         //<editor-fold desc="setup">
-        JFrame frame = new JFrame("testing");
+        frame = new JFrame("testing");
         frame.setSize(300,500-50);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(null);
@@ -33,6 +38,41 @@ public class gaem extends Thread {
         frame.add(gField.p.getScore());
         //</editor-fold>
         speed.start();
+    }
+
+    public static void set(gaem g) {
+        this = g;
+    }
+
+    public void reStart(){
+        //<editor-fold desc="setup">
+        frame = new JFrame("testing");
+        frame.setSize(300,500-50);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        block block = new block(new position(3,0));
+        block.randobblock(new position(3,0));
+
+        gField = new field(block, frame);
+
+
+
+        gField.setBackground(new Color(28, 28, 28));
+        gField.setBounds        (0,0,280,500);
+        gField.p.setBackground(new Color(146, 146, 146));
+        gField.p.setBounds      (200,0,80,50);
+        gField.p.getScore().setBounds(200,80,100,40);
+
+
+        frame.add(gField);
+        frame.add(gField.p);
+        frame.add(gField.p.getScore());
+        //</editor-fold>
+        speed = new speed();
+        speed.start();
+        start();
 
     }
     public void run(){
@@ -43,10 +83,30 @@ public class gaem extends Thread {
                     this.wait(speed.interval);
                 }catch (InterruptedException e){
                     throw new RuntimeException(e);
-
                 }
             }
 
         }
+        prompt p = new prompt(this);
+        p.setVisible(true);
+
     }
+}
+class prompt extends JFrame{
+    public prompt(gaem game){
+        super("Again?");
+        setBounds(0,0,100,70);
+        JButton button = new JButton("?");
+        button.setBounds(0,0,50,70);
+        add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                 gaem g = new gaem();
+                 gaem.set(g);
+            }
+        }
+    );
+    }
+
 }
